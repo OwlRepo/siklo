ab40704332bc26dedfd8d17e77678d837ef5118d
OwlRepo <romeoangeles010517@gmail.com>
2024-09-23 20:49:05 +0800

    feat: bottom-nav-bar setup

--- src/app/main/layout.js
+++ src/app/main/layout.js
@@ -1,10 +1,52 @@
-function MainLaout({ children }) {
+"use client";
+import { usePathname } from "next/navigation";
+import { Maps, Channels, Settings } from "../assets/icons";
+import Link from "next/link";
+
+function MainLayout({ children }) {
+  const pathname = usePathname();
+  const TAB_ROUTES = [
+    {
+      name: "Maps",
+      href: "/main/maps",
+      icon: <Maps stroke={pathname === "/main/maps" ? "#055FFC" : "black"} />,
+    },
+    {
+      name: "Channels",
+      href: "/main/channels",
+      icon: (
+        <Channels
+          stroke={pathname === "/main/channels" ? "#055FFC" : "black"}
+        />
+      ),
+    },
+    {
+      name: "Settings",
+      href: "/main/settings",
+      icon: (
+        <Settings
+          stroke={pathname === "/main/settings" ? "#055FFC" : "black"}
+        />
+      ),
+    },
+  ];
+
   return (
-    <div>
-      <p>main layout</p>
+    <div className="relative h-[100vh]">
       {children}
+      <div className="flex fixed bottom-0 w-full bg-white/70 h-[70px] justify-center space-x-14">
+        {TAB_ROUTES.map((tab) => (
+          <Link
+            key={tab.name}
+            href={tab.href}
+            className="flex flex-col items-center justify-center"
+          >
+            {tab.icon}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default MainLaout;
+export default MainLayout;
